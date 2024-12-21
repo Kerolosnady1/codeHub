2189affc4bfbc97ca239196d594792276fedf612
Kerolosnady1 <kerolosnady444@gmail.com>
2024-12-21 14:56:17 +0200

    added main

---
+++ codeHub.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    cout << "=======================================";
+    cout << "\n======= Exam Management System ========\n";
+    cout << "=======================================\n";
+    cout << "1. Manage User Data\n";
+    cout << "2. Add Questions\n";
+    cout << "3. Display Exam Data\n";
+    cout << "4. Create Exam\n";
+    cout << "5. Take Exam\n";
+    cout << "6. Grade Exam\n";
+    cout << "7. Save Data\n";
+    cout << "8. Load Data\n";
+    cout << "9. Display Results\n";
+    cout << "10. Exit\n";
+    cout << "Choose an option: ";
+}
